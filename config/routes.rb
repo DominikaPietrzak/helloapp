8344071dab95f3e5cb8377bd9d84db28696207c7
DominikaPieRep <dompie6@st.amu.edu.pl>
2018-05-04 15:59:38 +0200

    change gemfile, change controllers add sample controllers that render text witchout wievs , add decyfic routes taha render controllers for aplication

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'application#goodby'
+  #zrobić routing do akcji hello z jednoczestnym istnieniem akcji godby
+  #resource 'application#hello'
+  get 'hello' => 'application#hello'
 end
